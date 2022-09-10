317f275a634b6115fa48432a49a28b034e722859
herisusantoarismann <heri.s.arisman@gmail.com>
2022-09-10 12:19:35 +0700

    Add fun<button

--- react-fundamental/personal-notes-app-starter/src/pages/ArchivedPage.js
+++ react-fundamental/personal-notes-app-starter/src/pages/ArchivedPage.js
@@ -2,6 +2,7 @@ import React from "react";
 import NoteList from "../components/NoteList";
 import EmptyNotes from "../components/EmptyNotes";
 import { getArchivedNotes } from "../utils/local-data";
+import { FiPlus } from "react-icons/fi";
 
 const ArchivedPage = () => {
   const [notes, setNotes] = React.useState(getArchivedNotes());
@@ -15,18 +16,7 @@ const ArchivedPage = () => {
       {notes.length > 0 ? <NoteList notes={notes} /> : <EmptyNotes />}
       <div className="homepage__action">
         <button className="action" type="button" title="Tambah">
-          <svg
-            stroke="currentColor"
-            fill="currentColor"
-            strokeWidth="0"
-            viewBox="0 0 24 24"
-            height="1em"
-            width="1em"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path fill="none" d="M0 0h24v24H0V0z"></path>
-            <path d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z"></path>
-          </svg>
+          <FiPlus />
         </button>
       </div>
     </section>
--- react-fundamental/personal-notes-app-starter/src/pages/DetailPage.js
+++ react-fundamental/personal-notes-app-starter/src/pages/DetailPage.js
@@ -1,16 +1,28 @@
 import React from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { showFormattedDate } from "../utils";
-import { archiveNote, deleteNote, getNote } from "../utils/local-data";
+import {
+  archiveNote,
+  deleteNote,
+  getNote,
+  unarchiveNote,
+} from "../utils/local-data";
+import { BiArchiveIn, BiArchiveOut } from "react-icons/bi";
+import { FiTrash } from "react-icons/fi";
 
 const DetailPage = () => {
   const { idnote } = useParams();
   const notes = getNote(idnote);
-  const { id, title, createdAt, body } = notes;
+  const { id, title, createdAt, body, archived } = notes;
   const navigate = useNavigate();
 
   const onArchived = () => {
     archiveNote(id);
+    navigate("/");
+  };
+
+  const onUnarchived = () => {
+    unarchiveNote(id);
     navigate("/");
   };
 
@@ -25,43 +37,32 @@ const DetailPage = () => {
       <p className="detail-page__createdAt">{showFormattedDate(createdAt)}</p>
       <div className="detail-page__body">{body}</div>
       <div className="detail-page__action">
-        <button
-          className="action"
-          type="button"
-          title="Arsipkan"
-          onClick={() => onArchived()}
-        >
-          <svg
-            stroke="currentColor"
-            fill="currentColor"
-            strokeWidth="0"
-            viewBox="0 0 24 24"
-            height="1em"
-            width="1em"
-            xmlns="http://www.w3.org/2000/svg"
+        {archived ? (
+          <button
+            className="action"
+            type="button"
+            title="Arsipkan"
+            onClick={() => onUnarchived()}
           >
-            <path fill="none" d="M0 0h24v24H0V0z"></path>
-            <path d="M20.54 5.23l-1.39-1.68C18.88 3.21 18.47 3 18 3H6c-.47 0-.88.21-1.16.55L3.46 5.23C3.17 5.57 3 6.02 3 6.5V19c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V6.5c0-.48-.17-.93-.46-1.27zM6.24 5h11.52l.81.97H5.44l.8-.97zM5 19V8h14v11H5zm8.45-9h-2.9v3H8l4 4 4-4h-2.55z"></path>
-          </svg>
-        </button>
+            <BiArchiveOut />
+          </button>
+        ) : (
+          <button
+            className="action"
+            type="button"
+            title="Arsipkan"
+            onClick={() => onArchived()}
+          >
+            <BiArchiveIn />
+          </button>
+        )}
         <button
           className="action"
           type="button"
           title="Hapus"
           onClick={() => onDelete()}
         >
-          <svg
-            stroke="currentColor"
-            fill="currentColor"
-            strokeWidth="0"
-            viewBox="0 0 24 24"
-            height="1em"
-            width="1em"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path fill="none" d="M0 0h24v24H0V0z"></path>
-            <path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM8 9h8v10H8V9zm7.5-5l-1-1h-5l-1 1H5v2h14V4z"></path>
-          </svg>
+          <FiTrash />
         </button>
       </div>
     </section>
